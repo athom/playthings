97367d28e9dc807607870711d7c692d1851140cf
yeer <athom@126.com>
2012-01-16 21:30:40 +0800

    delete some old file

--- lib/turtletrack.rb
+++
@@ -1,5 +0,0 @@
-$: << File.dirname(__FILE__)
-
-require 'turtletrack/turtle.rb'
-require 'turtletrack/parser.rb'
-require 'turtletrack/game.rb'
--- test/test_helper.rb
+++
@@ -1,6 +0,0 @@
-$LOAD_PATH << '.'
-$LOAD_PATH.unshift(File.join( File.dirname(__FILE__), '..', 'lib' ))
-
-require 'test/unit'
-require 'turtletrack'
-include TurtleTrack
