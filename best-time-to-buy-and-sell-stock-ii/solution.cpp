b0625b5007bc6e52094393006acd05bba21e3664
athom <athom@126.com>
2013-12-02 15:43:31 +0800

    improve time performance for best-time-to-buy-and-sell-stock

--- best-time-to-buy-and-sell-stock-ii/solution.cpp
+++ best-time-to-buy-and-sell-stock-ii/solution.cpp
@@ -3,54 +3,36 @@
 using namespace std;
 
 
-struct P {
-    int price;
-    bool bought;
-};
-
+const int INVALID_NUM = -1;
 class Solution {
 public:
     int maxProfit(vector<int> &prices) {
-        std::vector<P> ps;
+        int sum = 0;
         for (vector<int>::iterator it = prices.begin(); it < prices.end(); it++)
         {
-            P p;
-            p.price = *it;
-            p.bought = false;
-            ps.push_back(p);
-        }
-
-        int sum = 0;
-        for (vector<P>::iterator it = ps.begin(); it < ps.end(); it++)
-        {
-            if (it->bought)
+            if (*it == INVALID_NUM)
             {
                 continue;
             }
-            it->bought = true;
 
-            int min_price = it->price;
-            vector<P>::iterator min_jt = it;
-            for (vector<P>::iterator jt = it+1; jt < ps.end(); jt++)
+            int min_price = *it;
+            vector<int>::iterator min_jt = it;
+            for (vector<int>::iterator jt = it+1; jt < prices.end(); jt++)
             {
-                if (jt->bought || jt->price > it->price)
+                if (*jt == INVALID_NUM || *jt > *it)
                 {
                     continue;
                 }
-                if (jt->price < min_price)
+
+                if (*jt < min_price)
                 {
-                    min_price = jt->price;
+                    min_price = *jt;
                     min_jt = jt;
                 }
             }
 
-            if(min_jt != it)
-            {
-                min_jt->bought = true;
-            }
-
-            sum += it->price - min_price;
-            
+            sum += (*it - *min_jt); 
+            *min_jt = INVALID_NUM;
         }
         return sum;    
     }
@@ -59,14 +41,14 @@ public:
 int main(int argc, char *argv[])
 {
     std::vector<int> v;
-    //v.push_back(5);
-    //v.push_back(3);
-    //v.push_back(2);
-    //v.push_back(3);
-    //v.push_back(1);
-    //v.push_back(3);
-    //v.push_back(2);
-    //v.push_back(3);
+    v.push_back(5);
+    v.push_back(3);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(2);
+    v.push_back(3);
 
     //v.push_back(1);
     //v.push_back(2);
@@ -77,14 +59,14 @@ int main(int argc, char *argv[])
     //v.push_back(7);
     //v.push_back(8);
 
-    v.push_back(8);
-    v.push_back(9);
-    v.push_back(6);
-    v.push_back(5);
-    v.push_back(9);
-    v.push_back(3);
-    v.push_back(6);
-    v.push_back(1);
+    //v.push_back(8);
+    //v.push_back(9);
+    //v.push_back(6);
+    //v.push_back(5);
+    //v.push_back(9);
+    //v.push_back(3);
+    //v.push_back(6);
+    //v.push_back(1);
 
     Solution s;
     cout << s.maxProfit(v) << endl;
