2da0a809bb71e9d5fc47f5a3359eaf389706d897
athom <athom@126.com>
2014-09-11 13:28:02 +0800

    improve lru cache

--- lru-cache/solution.cpp
+++ lru-cache/solution.cpp
@@ -1,81 +1,108 @@
 #include <iostream>
-#include <vector>
 #include <map>
+#include <assert.h>
 using namespace std;
 
+
+struct Node {
+    int key;
+    int value;
+    Node* pre;
+    Node* next;
+};
 
 
 class LRUCache{
 public:
     LRUCache(int capacity) {
-        this->mCap = capacity;
+        mCap = capacity;
+
+        mHead = NULL;
+        mTail = NULL;
     }
 
-    void _resetLessUsedKey(int key){
-        TLRUCacheMapIter it;
-        TLRUCacheMapIter minIt;
-        minIt = this->mMap.begin();
-        for (it = this->mMap.begin() ; it != this->mMap.end(); ++it)
+    int _moveNodeToHead(Node* node){
+        if (node == mHead)
         {
-            if (it->second->second < minIt->second->second)
-            {
-                minIt = it;
-            }
+            return mHead->value;
         }
-        this->mLessUserdKey = minIt->first;
+
+        if (node == mTail)
+        {
+            node->pre->next = NULL;
+            mTail = node->pre;
+        }
+        else{
+            node->pre->next = node->next;
+            node->next->pre = node->pre;
+        }
+
+        node->pre = NULL;
+        node->next = mHead;
+        mHead->pre = node;
+        mHead = node;
+        return mHead->value;
     }
 
     int get(int key) {
-        TLRUCacheMapIter it = this->mMap.find(key);
-        if (it == this->mMap.end())
+        TLRUCacheMapIter it = mMap.find(key);
+        if (it != mMap.end())
         {
-           return -1;
+            return _moveNodeToHead(it->second);
         }
 
-       it->second->second ++;
-       if (key == this->mLessUserdKey)
-       {
-            _resetLessUsedKey(key);
-       }
-       return it->second->first;
+        return -1;
     }
 
     void set(int key, int value) {
-        //cout << ">" << endl;
-        //cout << this->mMap.size() << endl;
-        TLRUCacheMapIter it = this->mMap.find(key);
-        if (it == this->mMap.end())
+        TLRUCacheMapIter it = mMap.find(key);
+        // hit the cache
+        if (it != mMap.end())
         {
-            if (this->mMap.size() == this->mCap)
-            {
-        cout << ">" << endl;
-                cout << this->mMap.size() << endl;
-                this->mMap.erase(this->mMap.find(mLessUserdKey));
-                cout << this->mMap.size() << endl;
-        cout << ">" << endl;
-            }
-
-            this->mMap[key] = new TLRUValuePair(value, 1);
-            _resetLessUsedKey(key);
+            it->second->value = value;
+            _moveNodeToHead(it->second);
             return;
         }
 
-        it->second->second ++;
-        if (key == this->mLessUserdKey)
+        Node* node = new Node;
+        node->key = key;
+        node->value = value;
+
+        // first element to set
+        if (mHead == NULL && mTail == NULL)
         {
-            _resetLessUsedKey(key);
+            mHead = node;
+            mTail = node;
+            mMap[key] = node;
+            return;
         }
-        it->second->first = value;
+
+        // append to head
+        node->next = mHead;
+        mHead->pre = node;
+        mHead = node;
+
+        // reach capacity, invalid the tail
+        if(mMap.size() == mCap) {
+            // remove tail element
+            Node* toBeTail = mTail->pre;
+            toBeTail->next = NULL;
+            mMap.erase(mTail->key);
+            delete mTail;
+            mTail = toBeTail;
+        }
+
+        mMap[key] = node;
     }
 
+    Node* mHead;
+    Node* mTail;
 private:
-    typedef std::pair<int, int> TLRUValuePair;
-    typedef std::map<int, TLRUValuePair*> TLRUCacheMap;
-    typedef std::map<int, TLRUValuePair*>::iterator TLRUCacheMapIter;
-    //typedef std::map<int, TLRUValuePair*>::iter ITLRUCacheMap;
-    TLRUCacheMap       mMap;
+    typedef std::map<int, Node*> TLRUCacheMap;
+    typedef std::map<int, Node*>::iterator TLRUCacheMapIter;
+
     int mCap;
-    int mLessUserdKey;
+    TLRUCacheMap       mMap;
 };
 
 int main(int argc, char *argv[])
@@ -84,21 +111,20 @@ int main(int argc, char *argv[])
     LRUCache c(3);
     c.set(1, 10);
     c.set(2, 20);
-    cout << c.get(2) << endl;
-    cout << c.get(2) << endl;
     c.set(3, 30);
     c.set(4, 40);
     c.set(4, 41);
+    c.set(3, 31);
     c.set(5, 50);
     c.set(6, 60);
 
-    cout << c.get(1) << endl;
-    cout << c.get(2) << endl;
-    cout << c.get(2) << endl;
-    cout << c.get(3) << endl;
-    cout << c.get(4) << endl;
-    cout << c.get(5) << endl;
-    cout << c.get(6) << endl;
+
+    assert(c.get(1) == -1);
+    assert(c.get(2) == -1);
+    assert(c.get(3) == 31);
+    assert(c.get(4) == -1);
+    assert(c.get(5) == 50);
+    assert(c.get(6) == 60);
 
     return 0;
 }
