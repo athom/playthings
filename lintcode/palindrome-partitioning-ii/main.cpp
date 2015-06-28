264fc45cbf5377763a80d1b0900cd0540353b027
athom <athom@126.com>
2015-06-28 23:28:00 +0800

    solve palindrome particioning ii in O(n^2)

--- lintcode/palindrome-partitioning-ii/main.cpp
+++ lintcode/palindrome-partitioning-ii/main.cpp
@@ -8,6 +8,40 @@ using namespace std;
 
 class Solution {
 public:
+    vector<vector<bool> > palindromeRange(string s) {
+        vector<vector<bool> > m;
+        int sz = s.size();
+        for (int i = 0; i < sz; ++i) {
+            vector<bool> v;
+            for (int i = 0; i < sz; ++i) {
+                v.push_back(false);
+            }
+            m.push_back(v);
+        }
+        for (int i = 0; i < sz; ++i) {
+            m[i][i] = true;
+        }
+
+        for (int c = 1; c < sz; ++c) {
+            for (int i = 0; i < sz; ++i) {
+                if (i+c >=sz) {
+                    continue;
+                }
+
+                if (s[i] != s[i+c]) {
+                    continue;
+                }
+
+                if (c == 1) {
+                    m[i][i+c] = true;
+                } else {
+                    m[i][i+c] = m[i+1][i+c-1];
+                }
+            }
+        }
+
+        return m;
+    }
     bool isPalindrome(string s) {
         int sz = s.size();
         for (int i = 0; i < sz; ++i) {
@@ -33,11 +67,11 @@ public:
         }
         v[0] = -1;
 
+        vector<vector<bool> > m = palindromeRange(s);
         for (int i = 1; i < sz + 1; ++i) {
             int sep =  ~(1<<31);
             for (int j = 0; j < i; ++j) {
-                string word = s.substr(j, i-j);
-                if (isPalindrome(word)) {
+                if (m[j][i-1]) {
                     if (v[j] + 1 < sep) {
                         sep = v[j] + 1;
                     }
@@ -55,6 +89,52 @@ int main(int argc, char *argv[])
 {
     Solution s;
     int r;
+    vector<vector<bool> > m;
+
+    m = s.palindromeRange("abcbabcb");
+    assert(m[0][0] == true);
+    assert(m[0][1] == false);
+    assert(m[0][2] == false);
+    assert(m[0][3] == false);
+    assert(m[0][4] == true);
+    assert(m[0][5] == false);
+    assert(m[0][6] == false);
+    assert(m[0][7] == false);
+
+    assert(m[1][1] == true);
+    assert(m[1][2] == false);
+    assert(m[1][3] == true);
+    assert(m[1][4] == false);
+    assert(m[1][5] == false);
+    assert(m[1][6] == false);
+    assert(m[1][7] == true);
+
+    assert(m[2][2] == true);
+    assert(m[2][3] == false);
+    assert(m[2][4] == false);
+    assert(m[2][5] == false);
+    assert(m[2][6] == true);
+    assert(m[2][7] == false);
+
+    assert(m[3][3] == true);
+    assert(m[3][4] == false);
+    assert(m[3][5] == true);
+    assert(m[3][6] == false);
+    assert(m[3][7] == false);
+
+    assert(m[4][4] == true);
+    assert(m[4][5] == false);
+    assert(m[4][6] == false);
+    assert(m[4][7] == false);
+
+    assert(m[5][5] == true);
+    assert(m[5][6] == false);
+    assert(m[5][7] == true);
+
+    assert(m[6][6] == true);
+    assert(m[6][7] == false);
+
+    assert(m[7][7] == true);
 
     r = s.minCut("aab");
     assert(r == 1);
