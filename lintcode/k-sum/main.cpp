efbd36710a3311a8a21589960487ccbce6a94526
athom <athom@126.com>
2015-07-05 14:40:48 +0800

    solve k sum more effective

--- lintcode/k-sum/main.cpp
+++ lintcode/k-sum/main.cpp
@@ -71,25 +71,90 @@ string matrix_to_str(vector<vector<Box> > m) {
     return os.str();
 }
 
+//class Solution {
+//public:
+    //Box findRemains(vector<vector<int> > remains, int target) {
+        //int sz = remains.size();
+        //Box b;
+        //b.sum = 0;
+        //for (int i = 0; i < sz; ++i) {
+            //for (int j = 0; j < remains[i].size(); ++j) {
+                //if (remains[i][j] == target) {
+                    //b.sum++;
+                    //remains[i].erase(remains[i].begin() + j);
+                    //b.remains.push_back(remains[i]);
+                    //break;
+                //}
+            //}
+        //}
+        //return b;
+    //}
+
+    ///**
+     //* @param A: an integer array.
+     //* @param k: a positive integer (k <= length(A))
+     //* @param target: a integer
+     //* @return an integer
+     //*/
+    //int kSum(vector<int> A, int k, int target) {
+        //int sz = A.size();
+        //if (k == 0 || target == 0 || sz == 0)  {
+            //return 0;
+        //}
+
+        //// Initicalize
+        //vector<vector<Box> > M;
+        //for (int i = 0; i <= k; ++i) {
+            //vector<Box> boxes;
+            //for (int j = 0; j <= target; ++j) {
+                //Box b;
+                //b.sum = 0;
+                //boxes.push_back(b);
+            //}
+            //M.push_back(boxes);
+        //}
+
+        //for (int i = 1; i <= k; ++i) {
+            //for (int j = i; j <= target; ++j) {
+                //Box box;
+                //box.sum = 0;
+                //for (int x = i-1; x < j; ++x) {
+                    //if ((i-1 == 0 && x == 0) || M[i-1][x].sum > 0) {
+                        //vector<vector<int> > remains = M[i-1][x].remains;
+                        //if (remains.size() == 0) {
+                            //remains.push_back(A);
+                       //}
+
+
+                        //Box b = findRemains(remains, j-x);
+                        //if (b.sum > 0) {
+                            //box.sum += b.sum;
+                            //for (int y = 0; y < b.remains.size(); ++y) {
+                                //bool found = false;
+                                //for (int z = 0; z < box.remains.size(); ++z) {
+                                    //if (box.remains[z] == b.remains[y]) {
+                                        //box.sum--;
+                                        //found = true;
+                                        //break;
+                                    //}
+                                //}
+                                //if (!found) {
+                                    //box.remains.push_back(b.remains[y]);
+                                //}
+                            //}
+                        //}
+                    //}
+                //}
+                //M[i][j] = box;
+            //}
+        //}
+
+        //return M[k][target].sum;
+    //}
+//};
+
 class Solution {
 public:
-    Box findRemains(vector<vector<int> > remains, int target) {
-        int sz = remains.size();
-        Box b;
-        b.sum = 0;
-        for (int i = 0; i < sz; ++i) {
-            for (int j = 0; j < remains[i].size(); ++j) {
-                if (remains[i][j] == target) {
-                    b.sum++;
-                    remains[i].erase(remains[i].begin() + j);
-                    b.remains.push_back(remains[i]);
-                    break;
-                }
-            }
-        }
-        return b;
-    }
-
     /**
      * @param A: an integer array.
      * @param k: a positive integer (k <= length(A))
@@ -98,59 +163,42 @@ public:
      */
     int kSum(vector<int> A, int k, int target) {
         int sz = A.size();
-        if (k == 0 || target == 0 || sz == 0)  {
+        if (sz == 0 || k == 0 || target == 0) {
             return 0;
         }
 
-        // Initicalize
-        vector<vector<Box> > M;
-        for (int i = 0; i <= k; ++i) {
-            vector<Box> boxes;
-            for (int j = 0; j <= target; ++j) {
-                Box b;
-                b.sum = 0;
-                boxes.push_back(b);
-            }
-            M.push_back(boxes);
-        }
-
-        for (int i = 1; i <= k; ++i) {
-            for (int j = 1; j <= target; ++j) {
-                Box box;
-                box.sum = 0;
-                for (int x = 0; x < j; ++x) {
-                    vector<vector<int> > remains = M[i-1][x].remains;
-                    if (remains.size() == 0) {
-                        remains.push_back(A);
-                    }
-                    if ((i-1 == 0 && x == 0) || M[i-1][x].sum > 0) {
-                        Box b = findRemains(remains, j-x);
-                        if (b.sum > 0) {
-                            box.sum += b.sum;
-                            for (int y = 0; y < b.remains.size(); ++y) {
-                                bool found = false;
-                                for (int z = 0; z < box.remains.size(); ++z) {
-                                    if (box.remains[z] == b.remains[y]) {
-                                        box.sum--;
-                                        found = true;
-                                        break;
-                                    }
-                                }
-                                if (!found) {
-                                    box.remains.push_back(b.remains[y]);
-                                }
-                            }
-                        }
-                    }
+        vector<vector<vector<int> > > M;
+        for (int i = 0; i <= sz; ++i) {
+            vector<vector<int> > vv;
+            for (int j = 0; j <= k; ++j) {
+                vector<int> v;
+                for (int k = 0; k <= target; ++k) {
+                    v.push_back(0);
                 }
-                M[i][j] = box;
+                vv.push_back(v);
+            }
+            M.push_back(vv);
+        }
+
+        for (int i = 0; i <= sz; ++i) {
+            M[i][0][0] = 1;
+        }
+
+        for (int i = 1; i <= sz; ++i) {
+            for (int j = 1; j <= i && j <= k; ++j) {
+                for (int y = 0; y <= target; ++y) {
+                    if (y - A[i-1] >= 0) {
+                        M[i][j][y] = M[i-1][j-1][y - A[i-1]];
+                    }
+                    int sum = M[i-1][j][y];
+                    M[i][j][y] += sum;
+                }
             }
         }
 
-        return M[k][target].sum;
+        return M[sz][k][target];
     }
 };
-        //cout << matrix_to_str(M) << endl;
 
 vector<int> seed_array(const int* a, int len) {
     vector<int> v;
@@ -179,5 +227,9 @@ int main(int argc, char *argv[])
     r = s.kSum(v, 2, 7);
     assert(r == 1);
 
+    int a2[] = {1,4,6,8,10, 13,15,17,18,21, 23,26,27,28,29, 30,32,35,36};
+    v = seed_array(a2, 19);
+    r = s.kSum(v, 9, 133);
+    assert(r == 231);
     return 0;
 }
